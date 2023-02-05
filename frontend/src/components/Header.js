52455721eb0782787e38aa7f93e8c17e2a7e5d0b
Alex Zakotyanskyi <sandrrichfif911@gmail.com>
2023-02-05 17:44:18 +0100

    style: FE v1.4 emoji update :-)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,8 @@ const Header = () => {
         <header>
             <h1>Welcome to Frontend of the TodoList application !</h1>
             <p>Source code can be found at the <a href="https://github.com/fif911/k8app">GitHub repo</a></p>
-            <p>Version 1.4</p>
+            <p>Version 1.4 🥳🥳🥳🥳🥳</p>
+            <p>This one cooler 😎😎😎😎😎😎</p>
         </header>
     )
 }
