4e917df41bd627d0877a210d58606656e166c4a8
Alex Zakotyanskyi <sandrrichfif911@gmail.com>
2023-02-05 10:48:50 +0100

    feat: website bomber for HPA testing

---
+++ website_bomber.py
@@ -0,0 +1,45 @@
+"""
+This file is needed to showcase the HPA in action
+Under stress the pods will be scaled until the scaling limit is reached or the load is handled
+"""
+import asyncio
+from aiohttp import ClientSession
+
+
+async def fetch(url, session):
+    async with session.get(url) as response:
+        delay = response.headers.get("DELAY")
+        date = response.headers.get("DATE")
+        print("{}:{} with delay {}".format(date, response.url, delay))
+        return await response.read()
+
+
+async def bound_fetch(sem, url, session):
+    # Getter function with semaphore.
+    async with sem:
+        await fetch(url, session)
+
+
+async def run(r):
+    url = "https://www.k8s.codes/api/items"
+    tasks = []
+    # create instance of Semaphore
+    sem = asyncio.Semaphore(1000)
+
+    # Create client session that will ensure we dont open new connection
+    # per each request.
+    async with ClientSession() as session:
+        for i in range(r):
+            # pass Semaphore and session to every GET request
+            task = asyncio.ensure_future(bound_fetch(sem, url, session))
+            tasks.append(task)
+
+        responses = asyncio.gather(*tasks)
+        await responses
+
+
+number = 1000000
+loop = asyncio.get_event_loop()
+
+future = asyncio.ensure_future(run(number))
+loop.run_until_complete(future)
