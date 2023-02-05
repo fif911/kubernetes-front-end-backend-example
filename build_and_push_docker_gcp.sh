b76c0884fd9472ead4422bc6004d778fd6ec50a0
nader2929 <nadersobhi1@hotmail.com>
2023-02-05 13:29:03 +0100

    GCP build script

---
+++ build_and_push_docker_gcp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [[ $# -ne 1 || $1 -ne "backend" || $1 -ne "frontend" ]]; then
+    echo "Script takes one arguement which must be either 'backend' or 'frontend'. Value given: '$1'"
+    exit 1
+fi
+
+echo "Running docker pipeline for: $1"
+echo ""
+
+echo "Building container"
+build_command="docker build $1 -f $1/$1.dockerfile -t kotonium/k8app-$1-image:latest"
+echo -e "\t$build_command"
+eval $build_command
+echo ""
+
+echo "Pushing container"
+push_command="docker push kotonium/k8app-$1-image:latest"
+echo -e "\t$push_command"
+eval $push_command
+echo ""
